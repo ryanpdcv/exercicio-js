6465562fe7a9db31a8f5c037a665c269c3d84628
Ryan <ryan.villar.com@gmail.com>
2025-02-04 23:00:53 -0300

    Alteração na function e no JS

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
-const form = document.getElementById('notas mes');
+const form = document.getElementById('notas-mes');
 
-function ValidaForm(){
-    primeiraNota < segundaNota;
+function ValidaForm(primeiraNota, segundaNota) {
+    let diferenca = segundaNota - primeiraNota;
+    return diferenca >= 1;
 }
-    
-form.addEventListener('submit',function(e){
-    e.preventDefault(); 
-    const primeiraNota = document.getElementById('primeiroMes');
-    const segundaNota = document.getElementById('segundoMes');
-    if (ValidaForm===false){
-        return alert('Não houve progressão')
-    }
-    else
-        return alert('Isso ai, belo progresso!')
-})
 
-console.log(form);
+form.addEventListener('submit', function(e) {
+    e.preventDefault(); 
+
+    const primeiraNota = Number(document.getElementById('primeiroMes').value);
+    const segundaNota = Number(document.getElementById('segundoMes').value);
+
+    if (!ValidaForm(primeiraNota, segundaNota)) {
+        window.alert('Você não teve progresso');
+    } else {
+        window.alert('Parabéns você teve progresso!');
+    }
+});
